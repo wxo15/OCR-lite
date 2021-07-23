8d5da80bb5b1e5a3767adf2e4c560619b70c3606
Max Ooi Wei Xiang <wxo_rocket0123@hotmail.com>
2021-07-24 03:43:42 +0800

    model eval backend

--- index.py
+++ index.py
@@ -2,31 +2,44 @@ from flask import Flask
 import os
 from flask import render_template, flash, request, redirect, url_for
 from joblib import dump, load
-
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__, template_folder='')
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST']) 
 def hello_world():
     if request.method == 'GET':
         return render_template('index.html')
-    else: #if method == 'POST'
-        img = request.image
-        return redirect(url_for('predict', imagedata=img))
-
+    else: #request.method == 'POST':
+        if request.json['image'] is None:
+            flash('No file part')
+            return redirect(request.url)
+        img = np.array(request.json['image'])
+        myModel = app.config['MODEL']
+        scaler = StandardScaler()
+        scaler.mean_ = app.config['MEAN']
+        scaler.var_ = app.config['VAR']
+        scaler.scale_ = app.config['SCALE']
+        img = scaler.transform(img.reshape(1, -1))
+        print(img)
+        res = myModel.predict(img)
+        print(res)
+        return render_template('index.html',result=res)    
 
 def load_model_from_file():
     myModel = load('logres.m5')
-    return myModel
-
-def predict(imgdata):
-    myModel = app.config['MODEL']
-    res = myModel.predict(imgdata)
-    return render_template('index.html',result=res)
+    myMean = np.load('logres_mean.npy')
+    myVar = np.load('logres_var.npy')
+    myScale = np.load('logres_scale.npy')
+    return (myModel, myMean, myVar, myScale)
 
 def main():
-    myModel = load_model_from_file()
+    (myModel, myMean, myVar, myScale) = load_model_from_file()
     app.config['MODEL'] = myModel
+    app.config['MEAN'] = myMean
+    app.config['VAR'] = myVar
+    app.config['SCALE'] = myScale
     app.run()
 
 main()
