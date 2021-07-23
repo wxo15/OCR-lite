0911af3c0a63cc968bca164582cd799a36f3f4c9
Max Ooi Wei Xiang <wxo_rocket0123@hotmail.com>
2021-07-23 23:57:02 +0800

    UI

--- index.py
+++ index.py
@@ -8,12 +8,21 @@ app = Flask(__name__, template_folder='')
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    else: #if method == 'POST'
+        img = request.image
+        return redirect(url_for('predict', imagedata=img))
 
 
 def load_model_from_file():
     myModel = load('logres.m5')
     return myModel
+
+def predict(imgdata):
+    myModel = app.config['MODEL']
+    res = myModel.predict(imgdata)
+    return render_template('index.html',result=res)
 
 def main():
     myModel = load_model_from_file()
